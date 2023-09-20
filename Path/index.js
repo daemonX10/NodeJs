9e8dbdd7af8dee53ccb1dca88adc7943eb008f87
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-20 17:45:38 +0530

    sage of six path

--- Path/index.js
+++ Path/index.js
@@ -1,1 +1,30 @@
 const path=require('path')
+
+// const filepath='D:\full stack developer\NodeJs\Path';
+
+// console.log("sep >",path.sep);
+
+// console.log("process.env.Path >",process.env.path);
+
+// console.log("delimeter >",path.delimiter);
+
+const currentFilePath=__filename;
+console.log("currentFilePath >",currentFilePath);
+
+// console.log(path.basename(currentFilePath,".js"));
+// console.log(path.dirname(currentFilePath));
+// console.log(path.extname(currentFilePath));
+// console.log(path.parse(currentFilePath));
+
+const pathToFile=path.format({
+    dir:'D:\\full stack developer\\NodeJs\\Path',
+    base:'index.js'
+});
+console.log(pathToFile);
+
+console.log('currentfilePath >',path.isAbsolute(currentFilePath));
+console.log("currentFilePath >",path.isAbsolute("../Path/index.js"));
+
+console.log('join >' , path.join("D:" ,"full stact developer","NodeJs","Path","inde.js"));
+
+console.log("path.relative >",path.relative)
