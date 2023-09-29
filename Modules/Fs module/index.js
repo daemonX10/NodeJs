570e5a40bd83c337b9c6e44055ab5960f7dd4ce6
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-30 00:02:30 +0530

    -m

--- Modules/Fs module/index.js
+++ Modules/Fs module/index.js
@@ -2,16 +2,31 @@ const fs = require('fs');
 
 const file='hello.txt'
 
-fs.readFile(file,(err,data)=>{
-    if(err){
+// fs.readFile(file,(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data.toString());
+//     }
+// });
+
+// console.log("Now sync way")
+
+// const data=fs.readFileSync(file);
+// console.log(data.toString()); // unless data will be in buffer formate
+
+// ( r, r+, rs, rs+, w, wx, w+, wx+,
+// a, ax, a+, ax+).
+
+const buf= new Buffer(1024);
+
+fs.open(file, 'r+', (err, fd) => {
+    if (err) {
         console.log(err);
-    }
-    else{
-        console.log(data.toString());
+    } else {
+        console.log("success in opening the file");
+        
+        fs.readFile(fd,buf)
     }
 });
-
-console.log("Now sync way")
-
-const data=fs.readFileSync(file);
-console.log(data.toString()); // unless data will be in buffer formate
