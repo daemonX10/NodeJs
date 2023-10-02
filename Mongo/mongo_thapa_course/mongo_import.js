6fbc59fdf48cd5821aec029b54b2112b6e9cfb9f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-03 03:31:20 +0530

    -z

--- Mongo/mongo_thapa_course/mongo_import.js
+++
@@ -1,6 +0,0 @@
-//* MONGOIMPORT
-// Now I will show How to import data from json file
-
-//? mongoimport E:\\mongo\products.json -d shop -c products
-
-//? mongoimport E:\\mongo\products.json -d shop -c products --jsonArray
