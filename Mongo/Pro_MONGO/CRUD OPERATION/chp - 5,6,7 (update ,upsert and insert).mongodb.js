14a0aa529dadc61ca8edecb9f7769044df3f0890
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-02 19:34:29 +0530

    completed

--- Mongo/Pro_MONGO/CRUD OPERATION/chp - 5,6,7 (update ,upsert and insert).mongodb.js
+++ Mongo/Pro_MONGO/CRUD OPERATION/chp - 5,6,7 (update ,upsert and insert).mongodb.js
@@ -13,3 +13,33 @@ db.mycol.insert({
 });
 
 db.mycol.find();
+
+// Chapter 7: Collections
+// Section 7.1: Create a Collection
+
+use("test");
+db.createCollection("mycol");
+
+// creatina a capped collection
+
+/* The code `db.createCollection("clappedCollection",{capped:true,size:10000,max:5000});` is creating a
+capped collection named "clappedCollection" in the current database. */
+db.createCollection("clappedCollection",{capped:true,size:10000,max:5000});
+
+db.cappedCollection.insert({name:"Damodar"});
+
+db.cappedCollection.createIndex({accountNo:1});
+
+db.adminCommand("listCollections");
+
+db.adminCommand("listDatabases");
+
+db.indexColl.find();
+
+db.getCollectionNames();
+
+db.cappedCollection.find();
+
+db.cappedCollection.drop();
+
+db.indexColl.drop();
