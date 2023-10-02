b127a212649559cfa587ceec8fc82a939fca4c9d
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-03 02:52:04 +0530

    f

--- Mongo/Pro_MONGO/CRUD OPERATION/chapter 8_Aggregation_.mongodb.js
+++ Mongo/Pro_MONGO/CRUD OPERATION/chapter 8_Aggregation_.mongodb.js
@@ -28,3 +28,37 @@ db.airnb.aggregate([
 ]);
 
 db.airnb.find();
+
+// 8.2 sum
+
+db.airnb.aggregate([
+    {
+        $group:{
+            _id:'$address.country',
+            count:{$sum:1}
+        }
+    }
+])
+
+// to find each thing present how many times
+
+// db.airnb.aggregate([
+//   {
+//     $unwind: "$amenities"
+//   },
+//   {
+//     $group: {
+//       _id: "$amenities",
+//       count: { $sum: 1 }
+//     }
+//   }
+// ])
+
+db.airnb.aggregate([
+  {
+    $project: {
+      amenitiesCount: { $size: "$amenities" }
+    }
+  }
+])
+
