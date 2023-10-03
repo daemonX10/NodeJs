f078a537eb7a70110fa4fac97d04e76c631fdd6c
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-03 20:20:43 +0530

    http module

--- Modules/Http Module/index.js
+++ Modules/Http Module/index.js
@@ -1,11 +1,16 @@
+const { link } = require('fs');
 const http= require('http');
 
-http.createServer((req,res)=>{
+const server=http.createServer((req,res)=>{
     if(req.url=='/'){
-        res.write("<h1>Hello everyone</h1>");
+        res.write("<h1>hello world</h1>");
+    }
+    else if(req.url=='/about'){
+        res.write("<h1>there is nothing , totally fuced up");
     }
     res.end();
 });
+
 
 server.listen(5005);
 
