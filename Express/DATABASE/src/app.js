b2626ebb60f5dd50d203d449351b99f722ba5f57
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-05 03:41:08 +0530

    f

---
+++ Express/DATABASE/src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('<h1>Twitter id </h1>')
+})
