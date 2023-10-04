f0b28ece05dd771b3df40397f57861e842ba6d5f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-05 01:14:41 +0530

    server via node

--- Express/Walkthrough With Express/index.js
+++ Express/Walkthrough With Express/index.js
@@ -4,9 +4,29 @@ const PORT=3011;
 const hostName="localhost";
 
 const server=http.createServer((req,res)=>{
-    res.statusCode=200;
-    res.setHeader("Content-Type","application/json")
-    res.end(JSON.stringify({"message":"Hello world"}));
+    if(req.url=='/'){
+        req.statusCode=200;
+        res.setHeader("Content-Type","application/json");
+        res.end(JSON.stringify({"message":"Hello world"}));
+    }
+    else if(req.url== '/about'){
+        req.statusCode=200;
+        res.setHeader("content-type","text/plain");
+        res.end(JSON.stringify("about page!"));
+
+    }
+    else if(req.url== '/contact'){
+        req.statusCode=200;
+        res.setHeader("Content-Type","Text/html");
+        res.end("<h1>contact page</h1>");
+
+    }
+    else{
+        res.statusCode=404;
+        res.setHeader("Content-Type","text/text");
+        res.end("Page not found");
+    
+    }
 });
 
 server.listen(PORT,hostName,()=>{
